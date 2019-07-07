6e69287ecb6cbb8e7f7d6583aec6d375bfc6d9d5
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2019-07-07 15:56:30 +0300

    Задача 1 уровня normal.

--- homeWork3/normal/tasks.py
+++ homeWork3/normal/tasks.py
@@ -10,3 +10,35 @@
 #  если скажем эти файлы потом придется передавать.
 # Так же при выводе имя должно быть полностью в верхнем регистре!
 # Подумайте вспоминая урок, как это можно сделать максимально кратко, используя возможности языка Python.
+
+employes_file_name = "salary.txt"
+
+print("\nЗадача-1\n")
+
+names_list = ["Василий", "Олег", "Татьяна", "Петр", "Андрей"]
+pays_list = [25000, 610000, 30000, 41000, 38000]
+
+employes_dict = dict(zip(names_list, pays_list))
+
+
+def write_employes_dict(dict1, file_name):
+    with open(file_name, "w", encoding='utf-8') as file:
+        for name, pay in dict1.items():
+            file.write(f"{name} - {pay}\n")
+
+
+def print_employes(file_name, upper_show_pay):
+    with open(file_name, "r", encoding='utf-8') as file:
+        for line in file:
+            line = line.strip()
+            name, pay = line.split(" - ")
+            name = name.upper()
+            pay = float(int(pay) - int(pay) * 13 / 100)
+            if pay <= upper_show_pay:
+                print(f"{name} - {pay:.2f}")
+
+
+print("Результирующий словарь:", employes_dict)
+write_employes_dict(employes_dict, employes_file_name)
+print("\nЗарплатная ведомость:")
+print_employes(employes_file_name, 500000)
