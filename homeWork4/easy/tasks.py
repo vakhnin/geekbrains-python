b5589eb96fe4be2048fab9c0eb0a02d6b8790cd8
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2019-07-11 12:04:22 +0300

    Задача 2 уровня easy.

--- homeWork4/easy/tasks.py
+++ homeWork4/easy/tasks.py
@@ -18,6 +18,33 @@ print("Список квадратов исходного списка:", new_ls
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
+import random
+
+print("\nЗадача-2\n")
+
+fruits_lst1 = ["apple", "avocado", "fig", "grapefruit", "grapes", "kiwi", "lime", "lemon",
+               "mango", "melon", "pear", "papaya", "pineapple", "peach", "plum", "watermelon"]
+
+fruits_lst2 = ["apple", "avocado", "apricot", "banana", "fig", "grapefruit", "mango", "melon",
+               "nectarine", "orange", "pear", "papaya", "pineapple", "peach", "plum", "watermelon"]
+
+
+def shake_lst(lst1, count):
+    for _ in range(count):
+        i = random.randint(0, len(lst1)-1)
+        j = random.randint(0, len(lst1)-1)
+        lst1[i], lst1[j] = lst1[j], lst1[i]
+
+
+# Перемешиваем списки фруктов. В условиях задачи не требовалось, но сделал.
+shake_lst(fruits_lst1, 10)
+print("Новый список фруктов номер 1:", fruits_lst1)
+shake_lst(fruits_lst2, 10)
+print("Новый список фруктов номер 2:", fruits_lst2)
+
+result_fruits_lst = [fruit for fruit in fruits_lst1 if fruit in fruits_lst2]
+print("Список фруктов, присутсвующих в обоих списках:", result_fruits_lst)
+
 # Задание-3:
 # Дан список, заполненный произвольными числами.
 # Получить список из элементов исходного, удовлетворяющих следующим условиям:
