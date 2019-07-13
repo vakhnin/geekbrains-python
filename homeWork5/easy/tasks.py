47f0a2afb0271bac2338f5891a772507ba8e11e7
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2019-07-13 14:47:42 +0300

    Задача 1 уровня easy.

--- homeWork5/easy/tasks.py
+++ homeWork5/easy/tasks.py
@@ -3,7 +3,45 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-# Задача-2:
+import os
+
+print("\nЗадача-1\n")
+
+path = os.getcwd()
+dirs_lst = ["dir_" + str(i) for i in range(1, 10)]
+
+
+def make_dirs(dirs_lst):
+    for my_dir in dirs_lst:
+        if not os.path.exists(my_dir):
+            try:
+                os.mkdir(my_dir)
+            except OSError:
+                print(f"Создать директорию {my_dir} не удалось.")
+            print(f"Успешно создана директория {my_dir}.")
+        else:
+            print(f"Дирректория {my_dir} уже существует.")
+
+
+def del_dirs(dirs_lst):
+    for my_dir in dirs_lst:
+        if os.path.exists(my_dir):
+            try:
+                os.rmdir(my_dir)
+            except OSError:
+                print(f"Удалить директорию {my_dir} не удалось.")
+            print(f"Успешно удалена директория {my_dir}.")
+        else:
+            print(f"Дирректория {my_dir} уже не существует.")
+
+
+# Если директория, первая в списке, существует, удаляем директории из списка, если не существует, создаем.
+if os.path.exists(dirs_lst[0]):
+    del_dirs(dirs_lst)
+else:
+    make_dirs(dirs_lst)
+
+# # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
 # Задача-3:
