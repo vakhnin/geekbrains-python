5e21bd83b5c40caeb8a6e36c82935121c6eb7591
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2019-07-17 16:57:29 +0300

    Задача 1 уровня normal.

--- homeWork6/normal/tasks.py
+++ homeWork6/normal/tasks.py
@@ -10,3 +10,65 @@
 # Функция подсчета урона должна быть инкапсулирована
 # Вам надо описать игровой цикл так же через класс.
 # Создайте экземпляры классов, проведите бой. Кто будет атаковать первым оставляю на ваше усмотрение.
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.health = 0
+        self.damage = 0
+        self.armor = 0
+
+    # @staticmethod
+    def _damage_on_armor(self, arm):
+        return self.damage / arm
+
+    def minus_damage(self, damage):
+        self.health -= damage
+
+    # @staticmethod
+    def attack(self, person):
+        person.minus_damage(self._damage_on_armor(person.armor))
+
+
+class Player(Person):
+    def __init__(self, name):
+        self.name = name
+        self.health = 110
+        self.damage = 30
+        self.armor = 1.3
+
+
+class Enemy(Person):
+    def __init__(self):
+        self.name = "Enemy"
+        self.health = 120
+        self.damage = 25
+        self.armor = 1.1
+
+
+player = Player(input("Введите имя игрока:"))
+enemy = Enemy()
+
+i = 1
+while True:
+    if player.health < 0 or enemy.health < 0:
+        break
+
+    print(f"Раунд {i}")
+    i += 1
+
+    print(f"Атакует {player.name}")
+    player.attack(enemy)
+    print(f"Атакует {enemy.name}")
+    enemy.attack(player)
+
+    print(f"Здоровье {player.name}: {int(player.health)}")
+    print(f"Здоровье {enemy.name}: {int(enemy.health)}\n")
+
+if player.health > 0:
+    print(f"\nПобедил игрок: {player.name}.")
+elif enemy.health > 0:
+    print(f"\nПобедил игрок: {enemy.name}.")
+else:
+    print("Оба игрока мертвы.")
