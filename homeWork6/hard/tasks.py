8934fe7182243d35525f20de95aea7bd83741aaf
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2019-07-18 16:49:42 +0300

    Задачи 1 и 2 уровня hard.

--- homeWork6/hard/tasks.py
+++ homeWork6/hard/tasks.py
@@ -11,3 +11,68 @@
 # Задача - 2
 # Доработайте нашу фабрику, создайте по одному классу на каждый тип, теперь надо в классе фабрика
 # исходя из типа игрушки отдавать конкретный объект класса, который наследуется от базового - Игрушка
+
+print("\nЗадача-1 и Задача-2\n")
+
+
+class Toy:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+class AnimalToy(Toy):
+    def __init__(self, name, color):
+        super().__init__(name, color)
+        self.toy_type = "animal_toy"
+
+
+class CartoonToy(Toy):
+    def __init__(self, name, color):
+        super().__init__(name, color)
+        self.toy_type = "cartoon_toy"
+
+
+toy_types = {
+    "animal_toy": AnimalToy,
+    "cartoon_toy": CartoonToy
+}
+
+
+class MakeToy:
+    def __init__(self, name, color, toy_type):
+        self.name = name
+        self.color = color
+        self.toy_type = toy_type
+        self.bought_materials = False
+        self.did_sewing = False
+        self.did_coloring = False
+
+    def buying_materials(self):
+        self.bought_materials = True
+        print("Материалы закуплены ...")
+
+    def doing_sewing(self):
+        if not self.bought_materials:
+            print("Материалы не закуплены. Пошив не возможен.")
+            return
+        self.did_sewing = True
+        print("Пошив завершен ...")
+
+    def doing_coloring(self):
+        if not self.did_sewing:
+            print("Пошив не завершен. Покраска не возможна.")
+            return
+        self.did_coloring = True
+        print("Игрушка изготовлена.")
+        return toy_types[self.toy_type](self.name, self.color)
+
+
+toy_fabric = MakeToy("Плюшевый медведь", "Коричневый", "animal_toy")
+
+toy_fabric.doing_sewing()
+toy_fabric.buying_materials()
+toy_fabric.doing_sewing()
+toy = toy_fabric.doing_coloring()
+
+print(f"\nИзготовлена игрушка {toy.name} цвета {toy.color} класса {toy.__class__}.")
